3c5e902ab1deb432d347200bba25b551f44859af
Isak Jacobsson <isak2jacobsson@gmail.com>
2025-10-18 13:07:24 +0200

    Configure and install all plugins from one command

--- init.lua
+++ init.lua
@@ -10,58 +10,14 @@ vim.g.have_nerd_font = true
 -- [[ Setting options ]]
 require 'set'
 
---e [[ Basic Keymaps ]]
+-- [[ Basic Keymaps ]]
 require 'remap'
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 require 'config.lazy'
 
--- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  To update plugins you can run
---    :Lazy update
---
--- NOTE: Here is where you install your plugins.
-require('lazy').setup({
-  require 'plugins.gitsigns',
-
-  require 'plugins.telescope',
-
-  require 'plugins.lspconfig',
-
-  require 'plugins.conform',
-
-  require 'plugins.blink',
-
-  require 'plugins.tokyonight',
-
-  require 'plugins.mini',
-
-  require 'plugins.treesitter',
-}, {
-  ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
-    },
-  },
-})
+-- [[ Configure and install all plugins ]]
+require('Lazy').setup 'plugins'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
