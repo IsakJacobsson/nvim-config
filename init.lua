0d777886493aa4b664fc603772fd2bc3dc34e292
Isak Jacobsson <isak2jacobsson@gmail.com>
2025-10-19 13:26:28 +0200

    Remove modeline from init.lua

--- init.lua
+++ init.lua
@@ -9,6 +9,3 @@ require("config.lazy")
 
 -- [[ Configure and install all plugins ]]
 require("Lazy").setup("plugins")
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
