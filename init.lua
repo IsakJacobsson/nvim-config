9043f3c630122facded9bd0bae6b3933c80b383c
Isak Jacobsson <isak2jacobsson@gmail.com>
2025-10-19 13:14:29 +0200

    Update stylua.toml

--- init.lua
+++ init.lua
@@ -1,14 +1,14 @@
 -- [[ Setting options ]]
-require 'config.set'
+require("config.set")
 
 -- [[ Basic Keymaps ]]
-require 'config.remap'
+require("config.remap")
 
 -- [[ Install `lazy.nvim` plugin manager ]]
-require 'config.lazy'
+require("config.lazy")
 
 -- [[ Configure and install all plugins ]]
-require('Lazy').setup 'plugins'
+require("Lazy").setup("plugins")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
