04c934c65c07e8219bda01d0a23e846264e54c73
Isak Jacobsson <isak2jacobsson@gmail.com>
2025-10-20 21:56:19 +0200

    Sensible keymaps for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -21,7 +21,7 @@ return {
                 },
                 pickers = {
                     find_files = {
-                        file_ignore_patterns = { "node_modules", ".git", ".venv" },
+                        file_ignore_patterns = { "node_modules", ".git", ".venv", "__pycache__" },
                         hidden = true,
                     },
                 },
@@ -29,10 +29,10 @@ return {
 
             -- See `:help telescope.builtin`
             local builtin = require("telescope.builtin")
-            vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+            vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
             vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-            vim.keymap.set("n", "<leader>pg", builtin.live_grep, {}) -- Requires ripgrep
-            vim.keymap.set("n", "<leader><leader>", builtin.buffers, {})
+            vim.keymap.set("n", "<leader>sg", builtin.live_grep, {}) -- Requires ripgrep
+            vim.keymap.set("n", "<leader>sb", builtin.buffers, {})
         end,
     },
 }
