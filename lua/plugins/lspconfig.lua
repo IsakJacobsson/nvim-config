86410043fcc590e069d6b71f0851ddb13bb87c00
Isak Jakobsson <isakj@axis.com>
2025-10-22 07:29:15 +0200

    Add Python and bash lsp servers
    Change-Id: I76cce51fb8e944ad9c78a7b3c884f168b5c4e4f6

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -223,7 +223,8 @@ return {
             local servers = {
                 -- clangd = {},
                 -- gopls = {},
-                -- pyright = {},
+                pyright = {},
+                bashls = {},
                 -- rust_analyzer = {},
                 -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
                 --
