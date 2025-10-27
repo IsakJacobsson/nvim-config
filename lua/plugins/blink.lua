d081c97b303d8c9b62e049e6939edd62bd1d2976
Isak Jakobsson <isakj@axis.com>
2025-10-27 11:20:53 +0100

    Blink.cmp: Disable menu scrollbar
    The scrollbar overlaps with the window boarder.
    
    Change-Id: Ief90bf613f64a59345e365deb07eeb9c295596e4

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -77,6 +77,8 @@ return {
                 -- By default, you may press `<c-space>` to show the documentation.
                 -- Optionally, set `auto_show = true` to show the documentation after a delay.
                 documentation = { auto_show = false, auto_show_delay_ms = 500 },
+                -- Scrollbar doesnt look good with menu border
+                menu = { scrollbar = false },
             },
 
             sources = {
