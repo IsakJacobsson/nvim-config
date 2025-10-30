7aebd7c238670540afce819e49f867e5fbf9db9e
Isak Jacobsson <isak2jacobsson@gmail.com>
2025-10-30 21:55:45 +0100

    Simplify lsp setup with mason-lspconfig plugin

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,5 +1,21 @@
 return {
-    -- Language server package manager
-    "mason-org/mason.nvim",
-    opts = {},
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = {
+        { "mason-org/mason.nvim", opts = {} },
+        "neovim/nvim-lspconfig",
+    },
+    config = function()
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "lua_ls",
+                "pyright",
+                "bashls",
+                "clangd",
+            }
+        })
+        -- Disable argument placeholders when completing function
+        vim.lsp.config("clangd", {
+            cmd = { "clangd", "--function-arg-placeholders=false" }
+        })
+    end
 }
