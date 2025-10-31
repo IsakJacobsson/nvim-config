ae2bd1e7441694e09442dc55422deba4c9711fd8
Isak Jakobsson <isakj@axis.com>
2025-10-31 07:59:38 +0100

    LSP: clean up mason plugin
    Change-Id: I1f6f19b2cd410970fa03435ea90c4940e1f30c21

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,21 +1,15 @@
 return {
     "mason-org/mason-lspconfig.nvim",
+    opts = {
+        ensure_installed = {
+            "lua_ls",
+            "pyright",
+            "bashls",
+            "clangd",
+        },
+    },
     dependencies = {
         { "mason-org/mason.nvim", opts = {} },
         "neovim/nvim-lspconfig",
     },
-    config = function()
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "lua_ls",
-                "pyright",
-                "bashls",
-                "clangd",
-            }
-        })
-        -- Disable argument placeholders when completing function
-        vim.lsp.config("clangd", {
-            cmd = { "clangd", "--function-arg-placeholders=false" }
-        })
-    end
 }
