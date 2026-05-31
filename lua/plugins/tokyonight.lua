93ef14a953a5daed83332f33f50005bb7ef48cdf
Isak Jacobsson <isak2jacobsson@gmail.com>
2026-05-31 20:57:03 +0200

    Add jaco colors to tokyonight theme

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -12,12 +12,128 @@ return {
                     -- Looks good with vim.o.winborder = true.
                     floats = "transparent",
                 },
+                on_colors = function(colors)
+                    colors.bg = "#edf3ee"
+                    colors.bg_dark = "#bed8cd"
+                    colors.bg_dark1 = "#93c0af"
+                    colors.bg_highlight = "#bed8cd"
+
+                    colors.bg_float = "#edf3ee"
+                    colors.bg_popup = "#edf3ee"
+                    colors.bg_sidebar = "#d7e4dd"
+                    colors.bg_statusline = "#d7e4dd"
+
+                    colors.border = "#929c98"
+                    colors.border_highlight = "#0076c5"
+
+                    colors.bg_visual = "#bed8cd"
+                    colors.bg_search = "#93c0af"
+
+                    colors.fg = "#000000"
+                    colors.fg_dark = "#000000"
+                    colors.fg_float = "#000000"
+                    colors.fg_sidebar = "#000000"
+                    colors.fg_gutter = "#929c98"
+
+                    colors.black = "#000000"
+                    colors.terminal_black = "#929c98"
+
+                    colors.blue = "#0076c5"
+                    colors.blue0 = "#3b87df"
+                    colors.blue1 = "#50b1b2"
+                    colors.blue2 = "#50b1b2"
+                    colors.blue5 = "#3b87df"
+                    colors.blue6 = "#50b1b2"
+                    colors.blue7 = "#93c0af"
+
+                    colors.cyan = "#008f8f"
+
+                    colors.green = "#008b17"
+                    colors.green1 = "#52b63d"
+                    colors.green2 = "#50b1b2"
+
+                    colors.yellow = "#c08403"
+                    colors.orange = "#dba137"
+
+                    colors.red = "#d12525"
+                    colors.red1 = "#d33d34"
+
+                    colors.magenta = "#7522d3"
+                    colors.magenta2 = "#752bdd"
+                    colors.purple = "#7522d3"
+
+                    colors.teal = "#008f8f"
+
+                    colors.comment = "#929c98"
+                    colors.dark3 = "#929c98"
+                    colors.dark5 = "#929c98"
+
+                    colors.diff = {
+                      add = "#bed8cd",
+                      change = "#93c0af",
+                      delete = "#d7e4dd",
+                      text = "#93c0af",
+                    }
+
+                    colors.git = {
+                      add = "#008b17",
+                      change = "#0076c5",
+                      delete = "#d12525",
+                      ignore = "#929c98",
+                    }
+
+                    colors.error = "#d12525"
+                    colors.warning = "#c08403"
+                    colors.info = "#0076c5"
+                    colors.hint = "#008f8f"
+                    colors.todo = "#0076c5"
+
+                    colors.terminal = {
+                      black = "#000000",
+                      black_bright = "#929c98",
+
+                      blue = "#0076c5",
+                      blue_bright = "#3b87df",
+
+                      cyan = "#008f8f",
+                      cyan_bright = "#50b1b2",
+
+                      green = "#008b17",
+                      green_bright = "#52b63d",
+
+                      magenta = "#7522d3",
+                      magenta_bright = "#752bdd",
+
+                      red = "#d12525",
+                      red_bright = "#d33d34",
+
+                      white = "#000000",
+                      white_bright = "#000000",
+
+                      yellow = "#c08403",
+                      yellow_bright = "#dba137",
+                    }
+
+                    colors.terminal_black = "#929c98"
+
+                    colors.none = "NONE"
+                    colors.rainbow = {
+                      "#0076c5",
+                      "#c08403",
+                      "#008b17",
+                      "#008f8f",
+                      "#7522d3",
+                      "#d33d34",
+                      "#3b87df",
+                      "#dba137",
+                    }
+                end,
             })
 
             -- Load the colorscheme here.
             -- Like many other themes, this one has different styles, and you could load
             -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-            vim.cmd.colorscheme("tokyonight-night")
+            vim.cmd.colorscheme("tokyonight-day")
         end,
     },
 }
