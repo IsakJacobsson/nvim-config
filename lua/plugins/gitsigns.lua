1555ad6dd48b257fddef60a52d1d9529373f6c25
Isak Jacobsson <isak2jacobsson@gmail.com>
2026-08-27 08:29:25 +0200

    Add git keybinds

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -10,6 +10,23 @@ return {
                 topdelete = { text = "‾" },
                 changedelete = { text = "~" },
             },
+            on_attach = function(bufnr)
+                local gitsigns = require("gitsigns")
+
+                local function map(keys, func, desc, mode)
+                    mode = mode or "n"
+                    vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = "Gitsigns: " .. desc })
+                end
+
+                map("<leader>gs", gitsigns.stage_hunk, "Stage hunk")
+                map("<leader>gr", gitsigns.reset_hunk, "Reset hunk")
+                map("<leader>gp", gitsigns.preview_hunk, "Preview hunk")
+                map("<leader>gi", gitsigns.preview_hunk_inline, "Preview hunk inline")
+                map("<leader>gb", function()
+                    gitsigns.blame_line({ full = true })
+                end, "Blame current line")
+                map("<leader>tb", gitsigns.toggle_current_line_blame, "Toggle line blame")
+            end,
         },
     },
 }
